293a6fa1883b5c1607220b9ee2a9d41b16fc2918
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-06-28 13:17:09 +0300

    Exercise 2.5 completed

--- Part2/courseinfo/src/App.jsx
+++ Part2/courseinfo/src/App.jsx
@@ -1,44 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <strong><p>Number of exercises {sum}</p></strong>
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part
-          part={part}
-          key={part.id}
-        />
-      ))}
-    </>
-  )
-}
-
-const Course = (props) => {
-  const parts = props.course.parts
-  const list_exercise = parts.map((part) => part.exercises)
-
-  const add = (accumulator, a) => accumulator + a
-  const sum = list_exercise.reduce(add, 0)
-  return (
-    <>
-      <Header course={props.course.name} />
-      <Content parts={parts} />
-      <Total sum={sum} />
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
@@ -88,10 +50,10 @@ const App = () => {
 
   return (
     <>
-
       <h1>Web development curriculum</h1>
-      {courses.map((course) => <Course course={course} key={course.id} />)}
-      {/* <Course course={courses} /> */}
+      {courses.map((course) =>
+        <Course course={course} key={course.id} />
+      )}
     </>
 
   )

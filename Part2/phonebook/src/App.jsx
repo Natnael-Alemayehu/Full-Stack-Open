39eb743c91aea0ccb8d107d28516bb6b5700b376
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-06-29 09:35:51 +0300

    Exercise 2.9 completed

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "0911201920" }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setFilter] = useState('')
 
   const handleChangeName = (event) => {
     // console.log(event.target.value)
@@ -16,11 +20,23 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleChangeFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+
+  const fil_persons = persons.filter((person) => {
+    return (
+      person.name.includes(newFilter)
+    )
+  })
+
   const handleOnSubmit = (event) => {
     event.preventDefault()
     const personObj = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     const nameExists = persons.some(person => person.name === newName)
 
@@ -43,6 +59,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input onChange={handleChangeFilter} />
+      </div>
+      <h2>Add New</h2>
+
       <form onSubmit={handleOnSubmit}>
         <div>
           <div>
@@ -51,8 +72,6 @@ const App = () => {
           <div>
             Number: <input onChange={handleChangeNumber} />
           </div>
-          <div>Name Change: {newName}</div>
-          <div>Number Change: {newNumber}</div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -60,9 +79,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      {persons.map((person) => {
+      {fil_persons.map((person) => {
         return (
-          <DisplayNames name={person.name} number={person.number} key={person.name} />
+          <DisplayNames name={person.name} number={person.number} key={person.id} />
         )
       })}
 
